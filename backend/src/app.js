40d221b87acee11192c6b5c1c7d68f8f9d351dee
yashG <yrg.creation.ai@gmail.com>
2026-03-24 21:42:31 +0530

    foodflix beta versio

---
+++ backend/src/app.js
@@ -0,0 +1,8 @@
+// create server on express
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("hello here is server live");
+})
+module.exports  = app;
